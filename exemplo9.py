38e1d73871fd0eb74ad05606226383f0c8d184a7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 11:39:29 -0300

    aulapython

--- exemplo9.py
+++ exemplo9.py
@@ -1,8 +1,7 @@
 nome = input("Insira seu nome: ")
-carros = int(input("Digite a quantidade de carros vendidos: ")
-salario1 = 2500.00
-valor = float (input("Insira o valo total de vendas: "))
-comissao = 200.00 * carros 
-porcentagem = 0.02 * valor 
-salariototal1 = salario1 + comissao + porcentagem 
-print(f'Nome do vendedor: {nome} \n Número de carros vendidos: {carros} \n Valor totl da vendas: {valor}') 
+carros = int (input("Digite a quantidade de carros vendidos: "))
+valor = float(input("Insira o valo total de vendas: "))
+comissao = 200 * carros 
+porcentagem = 0.02 * valor
+salariototal1 = 2500 + comissao + porcentagem 
+print(f'salario do vendedor {nome} é {salariototal1}') 
